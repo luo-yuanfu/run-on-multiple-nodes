eeadfabd425add37a8f669cd0da2cb1b61602e48
luo-yuanfu <varellow@gmail.com>
2017-01-05 14:06:25 +0800

    add importance comment in run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,11 @@
-# following are the available nodes
+# Following are the available nodes.
+# You need to config the names of those nodes in the ~/.ssh/config file.
+# For example:
+#	Host xcna11
+#	Hostname xcna11.comp.nus.edu.sg
+#	User yuanfu
+# Change the name 'yuanfu' to your account name
+# Also, to enable ssh without password, you need to use "ssh-copy-id" command to copy your public key to server
 nodes="";
 for n in $(seq 0 14); do nodes="$nodes xcna$n"; done
 for n in $(seq 0 19); do nodes="$nodes xcnb$n"; done
